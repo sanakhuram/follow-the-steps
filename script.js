e8a519ad906ea6a0478b6e64918f1109512c0f7e
Sana Khuram <126725476+sanakhuram@users.noreply.github.com>
2024-08-02 00:15:24 +0200

    added files added css styling for index and destination pages added header and footer

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const heroText = document.getElementById('heroText');
+    const text = heroText.textContent;
+    heroText.innerHTML = '';
+
+    const colors = ['#97A15C', '#6A7239', '#F2993F', '#D3F210']; // Array of colors to cycle through
+
+    text.split('').forEach((letter, index) => {
+        const span = document.createElement('span');
+        span.textContent = letter;
+        span.style.setProperty('--hover-color', colors[index % colors.length]); // Assign a color based on index
+        heroText.appendChild(span);
+    });
+});
+
