5df0657ba1897caa674e89e9006d5d0206576df0
Sana Khuram <126725476+sanakhuram@users.noreply.github.com>
2024-08-05 12:53:16 +0200

    added styling

--- forms.js
+++ forms.js
@@ -1,18 +1,23 @@
-function submitForm(event) {
+document.getElementById('showFormButton').addEventListener('click', function() {
+    document.getElementById('tourForm').style.display = 'block';
+    this.style.display = 'none';
+  });
+  
+  function submitForm(event) {
     event.preventDefault();
     const form = document.getElementById('tourForm');
     const confirmationMessage = document.getElementById('confirmationMessage');
-
+  
     // Display the confirmation message
     confirmationMessage.style.display = 'block';
-
+  
     // Clear the form
     form.reset();
-
+  
     setTimeout(() => {
-        confirmationMessage.style.display = 'none';
+      confirmationMessage.style.display = 'none';
     }, 5000);
-
+  
     return false;
-}
-
+  }
+  
